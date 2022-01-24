ceee7c91481f188842d0dc9ed0111b342fdcbc4b
yacine2027 <garoui.yacine2027@gmail.com>
2022-01-24 20:33:51 +0100

    third commit

---
+++ test.js
@@ -0,0 +1,9 @@
+const getUser = ()=> {
+    return [
+        {"name":"Abdou", "age":12},
+        {"name":"Touka", "age":7},
+        {"name":"Mohamed", "age":3}
+    ]
+}
+
+console.log(getUser())
